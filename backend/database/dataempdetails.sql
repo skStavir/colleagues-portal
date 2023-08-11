d4535ff795f1db979e792c32481e40df9df8a779
devipalliyil <devi007@gmail.com>
2023-08-11 14:14:43 +0530

    done

---
+++ backend/database/dataempdetails.sql
@@ -0,0 +1,5 @@
+INSERT INTO humaneoserver.dbo.humaneoemployees (emp_id,emp_name,Designation,ReportingManger,[Joining date],Email,[Action]) VALUES
+	 (1001,N'Santhosh',N'Manager',N'Santhosh','2023-01-06',N'santhosh.krishna@humaneo.in',N'dev'),
+	 (1002,N'Ajeesh',N'Cloud Engineer',N'Santhosh','2023-05-06',N'ajesh.ajayan@humaneo.in',N'cloud'),
+	 (1003,N'Devi',N'cloud azure',N'Santhosh','2023-11-07',N'devi.p@humaneo.in',N'azure cloud'),
+	 (1004,N'Krishnavamsi.Sidha',N'AWS Cloud',N'Santhosh','2023-11-07',N'krishnavamsi.sidda@humaneo.in',N'aws cloud');

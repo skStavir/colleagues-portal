58263988b0eac7ca7c1bd98a393a774da1f3c595
devipalliyil <devi007@gmail.com>
2023-08-11 13:01:02 +0530

    done

--- backend/database/data.sql
+++ backend/database/data.sql
@@ -1,5 +1,3 @@
-INSERT INTO humaneoserver.dbo.humaneoemployees (emp_id,emp_name,Designation,ReportingManger,[Joining date],Email,[Action]) VALUES
-	 (1001,N'Santhosh',N'Manager',N'Santhosh','2023-01-06',N'santhosh.krishna@humaneo.in',N'dev'),
-	 (1002,N'Ajeesh',N'Cloud Engineer',N'Santhosh','2023-05-06',N'ajesh.ajayan@humaneo.in',N'cloud'),
-	 (1003,N'Devi',N'cloud azure',N'Santhosh','2023-11-07',N'devi.p@humaneo.in',N'azure cloud'),
-	 (1004,N'Krishnavamsi.Sidha',N'AWS Cloud',N'Santhosh','2023-11-07',N'krishnavamsi.sidda@humaneo.in',N'aws cloud');
+INSERT INTO humaneoserver.dbo.timesheet (empname,[month],[Date],Worknghours,Leavehours,Holidayhours) VALUES
+	 (N'Devi',N'August','2023-08-08',8,0,0),
+	 (N'Ajesh',N'August','2023-08-08',8,0,0);

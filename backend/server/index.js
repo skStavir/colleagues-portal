385b3fe8dfa1b1146e50ca1fbb6743795877bb11
Devi <devi@stavir.com>
2023-10-19 10:07:43 +0530

    db integration of post API

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,52 +1,75 @@
-const express =require("express");
+const express = require("express");
 const app = express();
 const port = 3000;
-var bodyParser = require('body-parser');
+let bodyParser = require('body-parser');
 app.use(bodyParser.json());
-let timeesheet =[{
-    EmpName:"Devi",
-    Month:"august",
-    Date:"08-08-2023",
-    Workinghours:"8",
-    leavehours:"0", 
-    holidayhours:"0",  
-},
-{
-    EmpName:"Ajeesh",
-    Month:"August",
-    Date:"08-08-2023",
-    Workinghours:"8",
-    leavehours:"0",
-    holidayhours:"0",
-},
-];
-//get list
-app.get("/timeesheet" ,(req, res) =>{
-    res.json(timeesheet);
-} );
-
-//insert
-app.post("/timeesheet" ,(req, res) => {
-    var timeesheet1=req.body;
-    console.log(timeesheet1) ;
-    timeesheet.push(timeesheet1);
-    res.send("entry inserted");
+const mysql = require('mysql2')
+const connection = mysql.createConnection({
+    user: 'root',
+    host: '127.0.0.1',
+    database: 'emptime',
+    password: 'password@123',
+    port: 3307 
 });
-/*
-// Endpoint to update an employee by ID
-app.put('/timeesheet/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const updatedtimeesheet = req.body;
-  
-    timeesheet = timeesheet.map(timeesheet => {
-      if (timeesheet.id === id) {
-        return { ...timeesheet, ...updatedtimeesheet };
-      }
-      return timeesheet;
-    });
-  
-    res.json({ message: 'Employee updated successfully' });
-  });*/
+// Connect to the database
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to the database');
+});
 
-//lsten port
-app.listen(port, () => console.log(`server listenng at the port ${port}`));
+
+// app.get("/timesheet", (req, res) => {
+//     res.json(timesheet);
+// });
+
+// Insert data into the "empdata" table
+app.post("/fetchdata", (req, res) => {
+    const empData = req.body;
+    console.log(empData);
+
+    const query = {
+        text: 'INSERT INTO empdata(employee_id,employee_name, designation, phone_number, email, joining_date, leaving_date, reporting_manager_id,address) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        values: [
+            empData.employee_id,
+            empData.employee_name,
+            empData.designation,
+            empData.phone_number,
+            empData.email,
+            empData.joining_date,
+            empData.leaving_date,
+            empData.reporting_manager_id,
+            empData.address
+        ],
+    };
+
+    connection.query(
+        'INSERT INTO empdata(employee_id,employee_name, designation, phone_number, email, joining_date, leaving_date, reporting_manager_id, address) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        [
+            empData.employee_id,
+            empData.employee_name,
+            empData.designation,
+            empData.phone_number,
+            empData.email,
+            empData.joining_date,
+            empData.leaving_date,
+            empData.reporting_manager_id,
+            empData.address  
+        ],
+         (error, result) => {
+        if (error) {
+            console.error('Error inserting data:', error);
+            res.status(500).send('Error inserting data');
+        } else {
+            console.log('Data inserted successfully');
+            res.send("Entry inserted");
+
+            //console.log('Query results: ', results);
+        }
+    });
+});
+
+// Listen on the specified port
+app.listen(port, () => console.log(`Server listening at the port ${port}`));

2bd8766351cbd7d401223d08eb8989e846668f50
Devi <devi@stavir.com>
2023-10-30 18:07:00 +0530

    DB DATA

---
+++ backend/database/tabledata.sql
@@ -0,0 +1,6 @@
+INSERT INTO emptime.empdata (employee_id,employee_name,designation,phone_number,email,joining_date,leaving_date,reporting_manager_id,address) VALUES
+	 ('st01','Sangeetha','HR','9632170499','hr@stavir.com','2023-01-01','2027-01-02','Santhosh krisha','Banglore'),
+	 ('st02','Santhosh','Manager','9164746125','santhosh.krishna@stavir.com','2023-02-01','2026-01-12','Sangeetha','Banglore'),
+	 ('st03','Ajesh ajayan','aws engineer','8547089336','ajesh@stavir.com','2023-05-01','2024-01-12','Santhosh.K','kayamkulam'),
+	 ('st04','Devi','Azure Engineer','9496348256','devi@stavir.com','2023-07-11','2024-07-25','Santhosh krisha','guruvayur'),
+	 ('st05','Krishna vamsi','aws professional','6302700129','krishnavamsi@stavir.com','2023-07-01','2024-01-15','Santhosh','hyderabad');

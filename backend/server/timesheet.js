4e98dced7a71e5dc387de84d5e41545de8985b94
skStavir <145343451+skStavir@users.noreply.github.com>
2023-11-19 17:46:32 +0530

    split APIs into multiple files (#10)
    Co-authored-by: skStavir <santhosh.krishna@humaneo.in>

--- backend/server/timesheet.js
+++ backend/server/timesheet.js
@@ -1,134 +1,11 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
 const { v4: uuidv4 } = require('uuid');
-const mysql = require('mysql2/promise'); // Use the promise-based version of mysql2
-
-var PropertiesReader = require('properties-reader');
-var properties = PropertiesReader('config/app.properties');
-
-// Create a connection pool
-const dbConnectionPool = mysql.createPool({
-    user:properties.get("db.user"),
-    host: properties.get("db.server"),
-    database: properties.get("db.name"),
-    password: properties.get("db.password"),
-    port: properties.get("db.port"),
-    connectionLimit: properties.get("db.connectionLimit"), // Adjust the connection limit as needed
-});
-
-
-
-// Insert data into the "empdata" table
-app.post("/employees", async (req, res) => {
-    const empData = req.body;
-    console.log(empData);
-
-    // Generate a new UUID for the employee_id and convert it to a string
-    const employee_id = uuidv4().toString();
-    if (!empData.employee_name || !empData.designation || !empData.phone_number || !empData.email) {
-        res.status(400).send("Missing required fields.");
-        return;
-    }
-
-    try {
-        const connection = await dbConnectionPool.getConnection();
-        // Use a parameterized query to insert data into the database
-        const query = {
-            text: 'INSERT INTO empdata(employee_id, employee_name, designation, phone_number, email, joining_date, leaving_date, reporting_manager_id, address) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)',
-            values: [
-                employee_id,
-                empData.employee_name,
-                empData.designation,
-                empData.phone_number,
-                empData.email,
-                empData.joining_date,
-                empData.leaving_date,
-                empData.reporting_manager_id,
-                empData.address
-            ],
-        };
-
-        const [rows] = await connection.execute(query.text, query.values);
-        connection.release(); // Release the connection back to the pool
-        console.log('Data inserted successfully');
-        res.send("Entry inserted");
-    } catch (error) {
-        console.error('Error inserting data:', error);
-        res.status(500).send('Error inserting data');
-    }
-});
-
-// Define a route to get employee data
-app.get('/employees', async (req, res) => {
-    try {
-        const connection = await dbConnectionPool.getConnection();
-        const query = 'SELECT employee_id, employee_name, designation, phone_number, email, joining_date, leaving_date, reporting_manager_id, address FROM empdata';
-        const [results] = await connection.execute(query);
-        connection.release(); // Release the connection back to the pool
-        res.json(results);
-    } catch (error) {
-        console.error('Error querying the database:', error);
-        res.status(500).send('An error occurred while fetching employee data.');
-    }
-});
-// Define a route to update an employee's information
-app.put('/employees/:employee_id', async (req, res) => {
-    const employeeId = req.params.employee_id;
-    const {
-        employee_name,
-        designation,
-        phone_number,
-        email,
-        joining_date,
-        leaving_date,
-        reporting_manager_id,
-        address
-    } = req.body;
-
-    if (!employee_name || !designation || !phone_number || !email || !joining_date || !leaving_date || !reporting_manager_id || !address) {
-        res.status(400).json({ error: 'Bad request - Missing data' });
-        return;
-    }
-
-    try {
-        const connection = await dbConnectionPool.getConnection();
-        const query = `
-            UPDATE empdata 
-            SET employee_name = ?, designation = ?, phone_number = ?, email = ?, 
-            joining_date = ?, leaving_date = ?, reporting_manager_id = ?, address = ?
-            WHERE employee_id = ?
-        `;
-        const result = await connection.query(query, [
-            employee_name,
-            designation,
-            phone_number,
-            email,
-            joining_date,
-            leaving_date,
-            reporting_manager_id,
-            address,
-            employeeId
-        ]);
-
-        connection.release(); // Release the connection back to the pool
-
-        if (result[0].affectedRows > 0) {
-            res.status(200).json({ message: 'Employee updated successfully' });
-        } else {
-            res.status(404).json({ error: 'Employee does not exist' });
-        }
-    } catch (error) {
-        console.error('Error updating employee data: ' + error.message);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
+var dbConnectionPool = require('./db.js');
+const express = require('./parent.js')
+var timesheetAPIs = express.Router();
 
 //Timesheet APIS
 // Create a POST route for adding timesheet data
-app.post('/timesheet', async (req, res) => {
+timesheetAPIs.post('/', async (req, res) => {
     const { timesheet_id, employee_id, date, working_hours, leaves, holiday } = req.body;
 
     if (!timesheet_id || !employee_id || !date || working_hours === undefined || leaves === undefined || holiday === undefined) {
@@ -171,7 +48,7 @@ function isValidYearAndMonth(yearAndMonth) {
     return regex.test(yearAndMonth);
 }
 
-app.get('/employees/:employeeId/timesheet/month/:yearAndMonth', async (req, res) => {
+timesheetAPIs.get('/employees/:employeeId/month/:yearAndMonth', async (req, res) => {
     const employeeId = req.params.employeeId;
     const yearAndMonth = req.params.yearAndMonth;
 
@@ -217,7 +94,7 @@ app.get('/employees/:employeeId/timesheet/month/:yearAndMonth', async (req, res)
 
 
 //FETCH TIMESHEET BASED ON REPORTING MANAGER ID
-app.get('/employees/:reporting_manager_id/subordinates/timesheet/month/:yearAndMonth', async (req, res) => {
+timesheetAPIs.get('/employees/:reporting_manager_id/subordinates/month/:yearAndMonth', async (req, res) => {
     const { reporting_manager_id, yearAndMonth } = req.params;
 
     try {
@@ -290,5 +167,8 @@ app.get('/employees/:reporting_manager_id/subordinates/timesheet/month/:yearAndM
     }
 });
 
-// Listen on the specified port
-app.listen(port, () => console.log(`Server listening at the port ${port}`));
+
+
+module.exports=timesheetAPIs;
+
+

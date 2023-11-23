af9117f4b2e84898d92ca58730414822e3e40ad5
skStavir <santhosh.krishna@humaneo.in>
2023-11-23 11:13:18 +0530

    updated login

--- backend/server/login.js
+++ backend/server/login.js
@@ -8,98 +8,47 @@ loginAPIs.post('/', (req, res) => {
   console.log("loginAPI");
   const { username, password } = req.body;
   const connection =  dbConnectionPool.getConnection();
-  connection.query('SELECT * FROM empcred WHERE username = ?', [username], (err, results) => {
-    //connection.release();
-    console.log("line 18");
-    if (err) {
-      console.error('Error during login:', err);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
-    console.log('Results from SELECT query:', results);
+  const results = connection.query('SELECT * FROM empcred WHERE username = ?', [username]);
+  console.log("results" + results);
+  if (err) {
+    console.error('Error during login:', err);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+  console.log('Results from SELECT query:', results);
 
-    if (results.length === 0) {
-      return res.status(404).json({ error: 'Username not found' });
-    }
+  if (results.length === 0) {
+    return res.status(404).json({ error: 'Username not found' });
+  }
 
-    const user = results[0];
+  const user = results[0];
 
-    // Check if the password matches
-    if (user.password !== password) {
-      return res.status(401).json({ error: 'Invalid password' });
-    }
+  // Check if the password matches
+  if (user.password !== password) {
+    return res.status(401).json({ error: 'Invalid password' });
+  }
 
-    // Generate a new token
-    const newToken = uuidv4();
+  // Generate a new token
+  const newToken = uuidv4();
 
-    // Update the database with the new token and expiry time
-    connection.query(
-      'UPDATE empcred SET token = ?, expiryTime = NOW() + INTERVAL 1 HOUR WHERE username = ?',
-      [newToken, username],
-      (err, updateResults) => {
-        if (err) {
-          console.error('Error updating token:', err);
-          return res.status(500).json({ error: 'Internal server error' });
-        }
-
-        // If the update is successful, return the token
-        if (updateResults.affectedRows > 0) {
-          return res.status(200).json({ token: newToken });
-        } else {
-          return res.status(500).json({ error: 'Internal server error' });
-        }
-      }
-    );
-  });
-  /*dbConnectionPool.getConnection((err, connection) => { 
-    if (err) {
-      console.error('Error getting MySQL connection:', err);
-      return res.status(500).json({ error: 'Internal server error' });
-    }
-    console.log("line 16");
-    // Check if the username exists in the database
-    connection.query('SELECT * FROM empcred WHERE username = ?', [username], (err, results) => {
-      //connection.release();
-      console.log("line 18");
+  // Update the database with the new token and expiry time
+  connection.query(
+    'UPDATE empcred SET token = ?, expiryTime = NOW() + INTERVAL 1 HOUR WHERE username = ?',
+    [newToken, username],
+    (err, updateResults) => {
       if (err) {
-        console.error('Error during login:', err);
+        console.error('Error updating token:', err);
         return res.status(500).json({ error: 'Internal server error' });
       }
-      console.log('Results from SELECT query:', results);
 
-      if (results.length === 0) {
-        return res.status(404).json({ error: 'Username not found' });
+      // If the update is successful, return the token
+      if (updateResults.affectedRows > 0) {
+        return res.status(200).json({ token: newToken });
+      } else {
+        return res.status(500).json({ error: 'Internal server error' });
       }
-
-      const user = results[0];
-
-      // Check if the password matches
-      if (user.password !== password) {
-        return res.status(401).json({ error: 'Invalid password' });
-      }
-
-      // Generate a new token
-      const newToken = uuidv4();
-
-      // Update the database with the new token and expiry time
-      connection.query(
-        'UPDATE empcred SET token = ?, expiryTime = NOW() + INTERVAL 1 HOUR WHERE username = ?',
-        [newToken, username],
-        (err, updateResults) => {
-          if (err) {
-            console.error('Error updating token:', err);
-            return res.status(500).json({ error: 'Internal server error' });
-          }
-
-          // If the update is successful, return the token
-          if (updateResults.affectedRows > 0) {
-            return res.status(200).json({ token: newToken });
-          } else {
-            return res.status(500).json({ error: 'Internal server error' });
-          }
-        }
-      );
-    });
-  });*/
+    }
+  );
+  
 });
 
 // Handling invalid request payload

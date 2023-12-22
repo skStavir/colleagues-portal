bf24a1ee41cf1a2abb9adceb1df16696c41df345
devi-march <devi@stavir.com>
2023-12-22 18:11:22 +0530

    commited

---
+++ frontend/Login.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import './Login.css'; // Import your styling file
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/api/v1/login/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setToken(data.token);
+        console.log('Login successful. Token:', data.token);
+        setError(null); // Clear any previous errors
+      } else {
+        const errorData = await response.json(); // Assuming the error response is JSON
+        setError(errorData.error || 'Login failed'); // Extract the error message or provide a default message
+        console.error('Login failed:', errorData.error || response.statusText);
+      }
+    } catch (error) {
+      setError('Error during login. Please try again.'); // Generic error message for network or unexpected errors
+      console.error('Error during login:', error.message);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Submit
+        </button>
+      </form>
+      {error && <p className="error-message">{error}</p>}
+      {token && <p className="token-message">Token: {token}</p>}
+    </div>
+  );
+};
+
+export default Login;
---
+++ frontend/react readme.txt
@@ -0,0 +1,20 @@
+Ensure you have Node.js and npm installed on your machine
+
+Open your terminal (command prompt) and run the following command to install Create React App globally:
+npm install -g create-react-app
+This installs Create React App globally on your machine, allowing you to use it to create new React applications.
+
+
+
+Create a New React App:
+Once Create React App is installed, you can create a new React application by running the following command:
+npx create-react-app my-react-app
+
+Replace "my-react-app" with the desired name of your React application. 
+This command will create a new folder with the specified name and set up a basic React project structure inside it.
+
+Move into the newly created project directory:
+cd my-react-app
+
+Start the development server and open the React application in your default web 
+npm start

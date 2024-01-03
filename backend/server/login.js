eea6b38f771257f34a6308d8998cfe65d039f511
devi-march <devi@stavir.com>
2024-01-03 09:41:46 +0530

    commited

--- backend/server/login.js
+++ backend/server/login.js
@@ -94,63 +94,6 @@ loginAPIs.post('/', async (req, res) => {
   }
 });
 
-
-
-/*loginAPIs.post('/', async (req, res) => {
-  try {
-    // 1. Get user password
-    const { username, password } = req.body;
-    const connection = await dbConnectionPool.getConnection();
-
-    // 1.1 Create connection
-    // 1.2 Query db using connection
-    // 1.3 Get data
-    const query = {
-      text: 'SELECT * FROM empcred WHERE username = ?',
-      values: [username],
-    };
-
-    const [results] = await connection.execute(query.text, query.values);
-
-    if (results.length === 0) {
-      return res.status(404).json({ error: 'Username not found' });
-    }
-
-    const user = results[0];
-
-    // 2. Validate if the password is correct
-    if (user.password !== password) {
-      return res.status(401).json({ error: 'Invalid password' });
-    }
-
-    // 3. If invalid password, throw error (refer existing code)
-
-    // 4. Create token (refer existing code)
-    const newToken = uuidv4();
-
-    // 5. Save token to db
-    const updateSuccess = await updateTokenInDatabase(username, newToken);
-
-    // 6. Get additional employee information
-    const hasReporteesValue = await hasReportees(user.employee_id);
-
-    // 7. Return response with additional information
-    if (updateSuccess) {
-      return res.status(200).json({
-        token: newToken,
-        employee_id: user.employee_id,
-        employee_name: user.employee_name, // Assuming these columns exist in empcred table
-        hasReportees: hasReporteesValue,
-      });
-    } else {
-      return res.status(500).json({ error: 'Internal server error' });
-    }
-  } catch (error) {
-    console.error('Error during login:', error);
-    return res.status(500).json({ error: 'Internal server error' });
-  }
-});
-*/
 // Handling invalid request payload
 loginAPIs.use((err, req, res, next) => {
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
